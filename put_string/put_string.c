37b2b9ff2afdd3b83dfe70fba5563cd6b4f7fb85
Abdelali Ouhsine <abdaliouhssin@gmail.com>
2025-10-05 16:16:23 +0100

    put string

---
+++ put_string/put_string.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+
+void    ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void    put_string(char *str)
+{
+    int i;
+
+    i - 0;
+    while (str[i])
+    {
+        ft_putchar(str[i]);
+        i++;
+    }
+}
+
+int main()
+{
+    char str[] = "Shredder ego";
+    put_string(str);
+    return 0;
+}
