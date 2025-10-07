a010cdaba1ebaea4bb588cf0f810f68fa81a653a
Abdelali Ouhsine <abdaliouhssin@gmail.com>
2025-10-07 20:45:57 +0100

    c arguments

---
+++ print_programme_name/print_programme_name.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+void    ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+int     main(char argc, char *argv[])
+{
+    int i;
+
+    i = 0;
+    (void)argc;
+
+    while (argv[0][i])
+    {
+        ft_putchar(argv[0][i]);
+        i++;
+    }
+}
