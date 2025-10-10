579875a0badcbc3bd633e5624f86e20efc9ec849
Abdelali Ouhsine <abdaliouhssin@gmail.com>
2025-10-10 17:57:36 +0100

    010

---
+++ 17_pascal_s_trinagle/pascal_s_triangle.c
@@ -0,0 +1,60 @@
+/*
+Given an integer numRows, return the first numRows of Pascal's triangle.
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+ 
+
+Example 1:
+
+Input: numRows = 5
+Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+
+Example 2:
+
+Input: numRows = 1
+Output: [[1]]
+
+ 
+
+Constraints:
+
+    1 <= numRows <= 30
+*/
+
+
+int	power(int nb, int power)
+{
+	int	i;
+	int j;
+
+	i = 1;
+	j = nb;
+	if (power < 0)
+	{
+		return (0);
+	}
+	if (power == 0)
+	{
+		return (1);
+	}
+	while (i < power)
+	{
+		j = j * nb;
+		i++;
+	}
+	return (j);
+}
+
+int	pascals_triangle(int numrows)
+{
+	int i;
+	int nb;
+
+	nb = 1;
+	i = 0;
+	while(nb[i])
+	{
+	power(nb);
+	}
+}
