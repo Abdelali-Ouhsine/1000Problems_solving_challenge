80109dd567352a85708b548ebff9a752bab57974
Abdelali Ouhsine <abdaliouhssin@gmail.com>
2025-10-11 18:12:11 +0100

    combination

---
+++ 20_combination/combination.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Function to compute sum of first n numbers using dynamic programming
+int nSum(int n) {
+    int* dp = (int*)malloc((n + 1) * sizeof(int));
+    dp[0] = 0;
+
+    for (int i = 1; i <= n; i++) {
+        dp[i] = dp[i - 1] + i;
+    }
+
+    int result = dp[n];
+    free(dp); // Free the allocated memory
+    return result;
+}
+
+int main() {
+    int n = 10; // Example input
+    printf("Sum of first %d numbers is %d\n", n, nSum(n));
+    return 0;
+}
