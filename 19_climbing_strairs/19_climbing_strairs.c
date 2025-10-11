7c71f12f5f0dda37c4d35c965b7ebbd46e2f6b72
Abdelali Ouhsine <abdaliouhssin@gmail.com>
2025-10-11 17:47:49 +0100

    climbing strairs

---
+++ 19_climbing_strairs/19_climbing_strairs.c
@@ -0,0 +1,29 @@
+
+
+int    ft_way(int n)
+{
+	int dp [n + 1];
+	
+	if( n == 0 || n == 1)
+		return 1;
+
+	dp[0] = 1;
+	dp[1] = 1;
+	int i = 2;
+	while(i <= n)
+	{
+		dp[i] = dp[i - 1] + dp[i - 2];// thats call the rule of sum or addition principale
+		i++;
+	}
+		return dp[n];
+}
+
+#include <stdio.h>
+int main()
+{
+	    int nb = 3;
+        printf("enter your number\n");
+        scanf("%d",&nb);
+	    printf("the ways of the reach %d is %d\n",nb,ft_way(nb));
+	    return 0;
+}
