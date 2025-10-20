86bec924857c7a5095d3104f2eb01706e8380433
Abdelali Ouhsine <abdaliouhssin@gmail.com>
2025-10-20 22:50:59 +0100

    sort

---
+++ 25_sort_algo/sort_algo.c
@@ -0,0 +1,36 @@
+
+void	sort(int *tab, int size)
+{
+	int	i;
+	int	j;
+	int tmp;
+
+	i = 0;
+	j = 0;
+	while (i < size)
+	{
+		while (j < size)
+		{
+			if (tab[i] > tab[j])
+			{
+				tmp = tab[i];
+				tab[i] = tab[j];
+				tab[j] = tmp;
+			}
+			j++;
+		}
+		i++;
+		j = i + 1;
+	}
+}
+
+#include <stdio.h>
+int main()
+{
+	int tab[100] = {1,2,3,4};
+	int size = 4;
+
+	sort(tab,size);
+	printf("after process %ls",tab);
+	return 0;
+}
