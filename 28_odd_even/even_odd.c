fdb94af1a987bee67808aa2584987f5f62835b34
Abdelali Ouhsine <abdaliouhssin@gmail.com>
2025-10-21 18:20:36 +0100

    odd_even

---
+++ 28_odd_even/even_odd.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+int main()
+{
+	int nb;
+	printf("enter a number");
+	scanf("%d",&nb);
+	if(nb % 2 == 0)
+	{
+		printf("even number");
+	}
+	else
+	{
+		printf("odd number");
+	}
+}
