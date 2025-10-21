f5bc961f4e2f7918aa6b676ce0d8495350a60aaf
Abdelali Ouhsine <abdaliouhssin@gmail.com>
2025-10-21 18:39:50 +0100

    license check

---
+++ 29_dirver_license/driver_license.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+int main()
+{
+	int age;
+	printf("enter your age");
+	scanf("%d",&age);
+
+	if(age >= 21)
+	{
+		printf("you'r accepted for driver license");
+	}
+	else
+		printf("you'r Rejected");
+}
