c8f007b933eabc11945a72ad41d8299cc179498d
Abdelali Ouhsine <abdaliouhssin@gmail.com>
2025-11-02 22:27:34 +0100

    swap

--- 44_swap_numbers/swap_numbers.c
+++ 44_swap_numbers/swap_numbers.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 void	swap(int nb1,int nb2)
 {
-	int	temp;//create that temporary for store the valeus in the process
+	int	tempo;//create that temporary for store the valeus in the process
 		     
 	printf("enter to numbers\n");
 	scanf("%d %d",&nb1,&nb2);
 
-	temp = nb1;
+	tempo = nb1;
 	nb1 = nb2;
-	nb2 = temp;
+	nb2 = tempo;
 	//dispay that two numbers after swap
 	printf("the numbers after process nb1 :%d\n nb2:%d",nb1,nb2);
 }
