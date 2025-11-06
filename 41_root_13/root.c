8749fd8e086fd0841eed8b0b20e76d46d1daae8d
Abdelali Ouhsine <abdaliouhssin@gmail.com>
2025-11-06 12:04:22 +0100

    root13

--- 41_root_13/root.c
+++ 41_root_13/root.c
@@ -1,7 +1,7 @@
 #include <unistd.h>
 int main(int argc, char **argv)
 {
-	int i;
+	int	 i;
 	int	plus;
 	int minus;
 
@@ -27,6 +27,6 @@ int main(int argc, char **argv)
 			i++;
 		}
 	}
-	 write(1, "\n", 1);
+	 write(1, "\n\n", 1);
 	return (0);
 }
