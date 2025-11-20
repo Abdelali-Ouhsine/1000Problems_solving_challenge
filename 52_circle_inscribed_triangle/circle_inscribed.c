42f1a581fa9b43d91ac31d3553fe5eab298a4363
Abdelali Ouhsine <abdaliouhssin@gmail.com>
2025-11-20 04:53:51 -0800

    circle

---
+++ 52_circle_inscribed_triangle/circle_inscribed.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+
+void    inscribed_triangle()
+{
+
+    float   a;
+    float   b;
+    float   area;
+    float   pi = 3.14;
+
+    printf("enter a and b of the triangle");
+    scanf("%f %f", &a,&b);
+    area = ( pi * b * b /4) * ((2 * a - b) / (2 * a +b));
+    printf("thats the area of that ciecle%f", area);
+}
+
+int main()
+{
+    inscribed_triangle();
+    return 0;
+}
